573b8c356904462cd9b482860ac8542abb578f11
GustoVasconcelos <augusto@lothuscorp.com.br>
2025-10-30 22:56:03 -0300

    Implementado o modo dark do bootstrap no React

--- src/components/BtnVoltar.js
+++ src/components/BtnVoltar.js
@@ -2,6 +2,6 @@ import { Link } from "react-router-dom";
 
 export default function BtnVoltar() {
     return(
-        <Link className="btn btn-primary mt-1" to="/">Voltar</Link>
+        <Link className="btn btn-secondary mt-1" to="/">Voltar</Link>
     );
 }
