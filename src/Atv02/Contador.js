41312b2d7f4cf46639dfafb4937cd8a4483537b2
GustoVasconcelos <augusto@lothuscorp.com.br>
2025-11-05 09:43:18 -0300

    Componentizado o botao de reset e o contador do titulo da atividade02

--- src/Atv02/Contador.js
+++ src/Atv02/Contador.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ContadorItem from './components/ContadorItem';
 import './assets/css/Contador.css';
 import BtnVoltar from '../components/BtnVoltar';
+import ContadorTitulo from './components/ContadorTitulo';
+import ContadorBtnReset from './components/ContadorBtnReset';
 
 export default function Contador() {
     const [homens, setHomens] = useState(0);
@@ -26,16 +28,12 @@ export default function Contador() {
                 <div className="contador-central">
                     
                     <div className="contador-titulo">
-                        <h2 className="text-center flex-grow-1">Total</h2>
-                        <span className="badge text-bg-secondary fs-6 contador-total-badge">
-                            {total}
-                        </span>
-                        <button className="mt-2 btn btn-secondary" onClick={handleReset}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
-                                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
-                            </svg> Reset
-                        </button>
+                        <ContadorTitulo 
+                            tituloTotal={total}
+                        />
+                        <ContadorBtnReset 
+                            onReset={handleReset}
+                        />
                     </div>
 
                     <div className="d-flex flex-row align-items-start justify-content-center">
